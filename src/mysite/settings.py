6919543194cacaa9ea103c0f6ef57e7ace76d54d
ak-hayashi <eight.kayst@gmail.com>
2023-10-06 00:57:22 +0900

    Fix: HOSTをpostgresにしたら動いた。何も分からん

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         "NAME": "postgres",
         "USER": "postgres",
         "PASSWORD": "postgres",
-        "HOST": "",
+        "HOST": "postgres",
         "PORT": "5432",
     }
 }
